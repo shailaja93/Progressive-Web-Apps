bab61bdc5d06227309c741a17453e86e0744c255
Namrata Soni <namrata.soni@solutelabs.com>
2017-03-29 16:58:16 +0530

    Added Screenshots

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,18 @@
-var cacheName = 'cache_version3';
+var cacheName = 'cache_version4';
 var cacheFiles = [
-   'index.html'
+   'index.html',
+   './css/modern-business.css',
+   './font-awesome/css/font-awesome.min.css',
+   './font-awesome/fonts/FontAwesome.otf',
+   './font-awesome/fonts/fontawesome-webfont.eot',
+   './font-awesome/fonts/fontawesome-webfont.svg',
+   './font-awesome/fonts/fontawesome-webfont.ttf',
+   './font-awesome/fonts/fontawesome-webfont.woff',
+   './img/img-0.png',
+   './img/no-internet.png',
+   './img/pwa.png',
+   './img/service-worker.png',
+   './img/web-app.jpeg'
 ];
 self.addEventListener('install',function(e){
   //  console.log("[Service Worker] Installed");
@@ -15,13 +27,13 @@ self.addEventListener('install',function(e){
 })
 
 self.addEventListener('activate', function(e){
-  //  console.log("[Service Worker] Activated");
+   console.log("[Service Worker] Activated");
    e.waitUntil(
     caches.keys().then(function(cacheNames){
      return Promise.all(cacheNames.map(function(thisCacheName){
      if(thisCacheName != cacheName)
      {
-      // console.log("[Service Worker] Removing Cached Files from", thisCacheName);
+      console.log("[Service Worker] Removing Cached Files from", thisCacheName);
       return caches.delete(thisCacheName);
      }
      }))
